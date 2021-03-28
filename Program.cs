65cd1ed4626bafa0e151f58087d7fc2aff653289
ht-evth <ht-evth@users.noreply.github.com>
2021-03-28 20:19:53 +0700

    Revert "Принял изменения"
    This reverts commit e5dba6584e8c320d37faa26ea4ce579e85ba2324.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace OGM {
 		static void Main() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-
+			Application.Run(new LogInForm());
 		}
 	}
 }
